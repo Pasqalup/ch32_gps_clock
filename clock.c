3b0088a1f5a15dd62c4a69226b8015afb8f93624
Luke Zhao <pasqalup25@gmail.com>
2026-04-30 19:13:25 +0000

    add alarm

--- clock.c
+++ clock.c
@@ -3,10 +3,39 @@
 #include "clock_i2c.h"
 #include "uart.h"
 
+//GPS update rate is set to 0.1Hz, so we should get a pulse every 10 seconds
 #define PPS_PORT GPIOC
 #define PPS_PIN 5
 #define GPS_SET_RMC_ONLY "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29"
 #define GPS_SET_0_1HZ "$PMTK220,10000*2F"
+
+//alarm buzzer pin
+#define BUZZER_PORT GPIOB
+#define BUZZER_PIN 0
+
+// which buttons are we looking for?
+#define BUTTON_1_MASK 0x01
+#define BUTTON_2_MASK 0x02
+
+// macros for systick timing functions
+#define millis() (systick_millis)
+#define micros() (SysTick->CNT / DELAY_US_TIME)
+
+// millisecond systick timer
+volatile uint32_t systick_millis;
+
+// save last button state to detect changes
+uint8_t last_button_state = 0; 
+// handles long button presses
+uint32_t last_button1_press_time = 0;
+uint32_t last_button2_press_time = 0;
+
+// global variable to store alarm time; format is 0xHHMM, where HH is hours and MM is minutes
+uint16_t alarm_time = 0; // byte 1: hours, byte 2: minutes
+// global time variables for clock time
+uint8_t hours, minutes, seconds;
+// all flags - [pps triggered][colon on][alarm stop]
+volatile uint8_t flags;
 
 typedef enum
 {
@@ -15,9 +44,17 @@ typedef enum
 	CLOCK_READY,
 } State_t;
 State_t state = GPS_SETUP;
-uint8_t hours, minutes, seconds;
-// flags - bit 0: pps irq flag bit 1: colon on/off flag
-volatile uint8_t flags;
+typedef enum
+{
+	REGULAR_TIME,
+	ALARM_SET_DIG1,
+	ALARM_SET_DIG2
+} TimeMode_t;
+TimeMode_t time_mode = REGULAR_TIME;
+void configure_buzzer(){
+	RCC->APB2PCENR |= RCC_APB2Periph_TIM1; // enable clock for timer 1
+	RCC->APB2PCENR |= RCC_APB2Periph_GPIOB; // enable clock for GPIOB
+}
 void configure_pps(){
     // configure pin as input
     GPIOC->CFGLR &= ~(0x0F << (4 * 5));
@@ -34,6 +71,8 @@ void configure_pps(){
 
 }
 
+
+// pps interrupt handler - precision second pulse
 void EXTI7_0_IRQHandler( void ) __attribute__((interrupt));
 void EXTI7_0_IRQHandler( void ) 
 {
@@ -45,6 +84,56 @@ void EXTI7_0_IRQHandler( void )
 		seconds++; // increment seconds; the main loop will handle rolling over minutes and hours and updating the
 		           // display
     }
+}
+void systick_init(void)
+{
+	// Reset any pre-existing configuration
+	SysTick->CTLR = 0x0000;
+	
+	// Set the compare register to trigger once per millisecond
+	SysTick->CMP = DELAY_MS_TIME - 1;
+
+	// Reset the Count Register, and the global millis counter to 0
+	SysTick->CNT = 0x00000000;
+	systick_millis = 0x00000000;
+	
+	// Set the SysTick Configuration
+	// NOTE: By not setting SYSTICK_CTLR_STRE, we maintain compatibility with
+	// busywait delay funtions used by ch32v003_fun.
+	SysTick->CTLR |= SYSTICK_CTLR_STE   |  // Enable Counter
+	                 SYSTICK_CTLR_STIE  |  // Enable Interrupts
+	                 SYSTICK_CTLR_STCLK ;  // Set Clock Source to HCLK/1
+	
+	// Enable the SysTick IRQ
+	NVIC_EnableIRQ(SysTick_IRQn);
+}
+void SysTick_Handler(void) __attribute__((interrupt));
+void SysTick_Handler(void)
+{
+	// Increment the Compare Register for the next trigger
+	// If more than this number of ticks elapse before the trigger is reset,
+	// you may miss your next interrupt trigger
+	// (Make sure the IQR is lightweight and CMP value is reasonable)
+	SysTick->CMP += DELAY_MS_TIME;
+
+	// Clear the trigger state for the next IRQ
+	SysTick->SR = 0x00000000;
+
+	// Increment the milliseconds count
+	systick_millis++;
+}
+
+void handle_alarm(){
+	// if we are in regular time mode and the current time matches the alarm time, buzz
+	// if we press snooze, stop buzzing
+	if( time_mode == REGULAR_TIME && ( ( hours << 8 ) | minutes ) == alarm_time && !( flags & 0x04 ) ){
+		// buzz on and off every 500ms
+		if( ( millis() / 500 ) % 2 ){
+			// turn buzzer on
+		} else{
+			// turn buzzer off
+		}
+	}
 }
 int main(){
     SystemInit();
@@ -134,34 +223,90 @@ int main(){
 				break;
 
             case CLOCK_READY:
-				if ( flags & 0x01 ) // if we have a pps event, update the display
-				{
-					// clear the pps flag
-					flags &= ~0x01;
-					// toggle colon flag
-					flags ^= 0x02;
-					// handle rolling over seconds, minutes, hours
-					if ( seconds >= 60 )
-					{
-						seconds = 0;
-						minutes++;
-						if ( minutes >= 60 )
-						{
-							minutes = 0;
-							hours++;
-							if ( hours >= 24 )
-							{
-								hours = 0;
-							}
+				ch455_read_key(); //update button state;
+				uint8_t button_state = ch455_get_key();
+				//detect rising edge
+				uint8_t changed_buttons = ( button_state ^ last_button_state ) & button_state;
+				uint8_t rising_edges = changed_buttons & button_state;
+				uint8_t falling_edges = changed_buttons & ~button_state;
+				if( rising_edges & BUTTON_1_MASK ){
+					last_button1_press_time = millis();
+				} else if( rising_edges & BUTTON_2_MASK ){
+					last_button2_press_time = millis();
+				}
+				// if button 1 is held go into alarm set mode
+				// if not it is used to advance digits in alarm set mode
+				if (falling_edges & BUTTON_1_MASK) {
+					if( millis() - last_button1_press_time >= 3000 ){ // if button was held for at least 3 seconds
+						time_mode = ALARM_SET_DIG1;
+					} else{
+						//regular press to advance digits in alarm set mode
+						if( time_mode == ALARM_SET_DIG1 ){
+							time_mode = ALARM_SET_DIG2;
+						} else if( time_mode == ALARM_SET_DIG2 ){
+							time_mode = REGULAR_TIME;
 						}
 					}
-					// update the display with the current time
-					ch455_writeclock( hours, minutes, flags & 0x02 );
+					if(millis-last_button1_press_time >=10000){
+						// 10 seconds press rereads gps data
+						state = UART_WAIT_FOR_RX; 
+					}
 				}
-				ch455_read_key(); //update button state;
+				if(falling_edges & BUTTON_2_MASK){ // release button 2 to increment current digit
+					if( time_mode == ALARM_SET_DIG1 ){ // set hours
+						alarm_time += 0x0100; // increment the hour byte
+						if( ( alarm_time >> 8 ) > 23 ){ // roll over if hours exceed 23
+							alarm_time &= 0x00FF; // reset hour byte to 0
+						}
+					} else if( time_mode == ALARM_SET_DIG2 ){ //set minutes
+						alarm_time += 0x01; // increment the minute byte
+						if( ( alarm_time & 0x00FF ) > 59 ){ // roll over if minutes exceed 59
+							alarm_time &= 0xFF00; // reset minute byte to 0
+						}
+					}
 
+					// buttons 2 acts as snooze/alarm stop
+					flags |= 0x04; // set bit 2 to indicate alarm stop/snooze
+				}
 
+				last_button_state = button_state; // store button state for next iteration to detect changes
 		
+				switch ( time_mode )
+				{
+					case REGULAR_TIME: // display regular time
+						if ( flags & 0x01 ) // if we have a pps event, update the display
+						{
+							// clear the pps flag
+							flags &= ~0x01;
+							// toggle colon flag
+							flags ^= 0x02;
+							// handle rolling over seconds, minutes, hours
+							if ( seconds >= 60 )
+							{
+								seconds = 0;
+								minutes++;
+								if ( minutes >= 60 )
+								{
+									minutes = 0;
+									hours++;
+									if ( hours >= 24 )
+									{
+										hours = 0;
+									}
+								}
+							}
+							// update the display with the current time
+							ch455_writeclock( hours, minutes, flags & 0x02 );
+						}
+						break;
+					case ALARM_SET_DIG1: // display alarm hour with blinking colon
+						ch455_writeclock( ( alarm_time >> 8 ) & 0xFF, ( alarm_time & 0x00FF ), ( millis() / 500 ) % 2 ); // blink colon every 500ms
+						break;
+					case ALARM_SET_DIG2: // display alarm minute with solid colon
+						ch455_writeclock( ( alarm_time >> 8 ) & 0xFF, ( alarm_time & 0x00FF ), 1 ); // solid colon
+						break;
+
+				
         }
     }
 
